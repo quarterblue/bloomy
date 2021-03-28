caf47628fc9c52f546fdea722f43eaf0e64e1c80
quarterblue <quarterslate@gmail.com>
2021-03-27 20:44:36 -0700

    Parse stock, need refactor the cloning

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+extern crate reqwest;
+// use reqwest::Error;
+use std::env;
+
+enum Currency {
+    CAD,
+    USD,
+}
+
+struct StockArgs {
+    stock: String,
+    chart: String,
+    datalog: String,
+    currency: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _client = reqwest::Client::new();
+    let args: Vec<String> = env::args().skip(1).collect();
+
+    let stockArgs = parse_stock(&args);
+
+    println!("{:?}", args);
+}
+
+fn parse_stock(args: &[String]) -> StockArgs {
+    let stock = args[0].clone();
+    let chart = args[1].clone();
+    let datalog = args[2].clone();
+    let currency = args[3].clone();
+
+    StockArgs {
+        stock,
+        chart,
+        datalog,
+        currency,
+    }
 }
