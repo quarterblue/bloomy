eee6a995cea610714520d6d512edb03c784e6686
quarterblue <quarterslate@gmail.com>
2021-03-28 18:15:20 -0700

    Add help usage

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,77 @@
 extern crate reqwest;
 // use reqwest::Error;
 use std::env;
+use std::str::FromStr;
+
+/**
+ * UsageList :
+ *
+ * Single stock w/o command:
+ *      bloomy AAPL
+ *      bloomy apple
+ *      bloomy tesla
+ *
+ * Single stock command w/ options:
+ *  With chart option:
+ *      bloomy apple -c
+ *  With chart and long datalog:
+ *      bloomy apple -cd
+ */
+
+struct Config {
+    url: String,
+    key: String,
+}
 
 enum Currency {
     CAD,
     USD,
+    DEFAULT,
 }
 
 struct StockArgs {
     stock: String,
-    chart: String,
-    datalog: String,
-    currency: String,
+    chart: bool,
+    datalog: bool,
+    currency: Currency,
 }
 
-fn main() {
+impl StockArgs {
+    fn new(args: &[String]) -> Result<StockArgs, &'static str> {
+        if args.len() == 1 {
+            let flag = args[1].clone();
+            if flag.contains("-h") || flag.contains("-help") {
+                println!(
+                    "Program Usage: bloomy stockname options
+                \r\n      -h or -help to show help message
+                \r\n      -c to show full charts
+                \r\n      -d to show full data logs
+                \r\n      -USD to show currency in us dollar 
+                \r\n      -CAD to show currency in canadian dollar"
+                );
+                return Err("help");
+            } else {
+                return Ok(StockArgs {
+                    stock: args[1].clone(),
+                    chart: false,
+                    datalog: false,
+                    currency: Currency::DEFAULT,
+                });
+            }
+        }
+        if args.len() < 1 {
+            return Err("Program Usage: bloomy stockname options");
+        } else if args.len() > 5 {
+            return Err("Too many arguments.");
+        }
+        let options = args[2].clone();
+    }
+}
+
+fn main() -> Result<(), std::io::Error> {
     let _client = reqwest::Client::new();
     let args: Vec<String> = env::args().skip(1).collect();
 
-    let stockArgs = parse_stock(&args);
-
     println!("{:?}", args);
-}
-
-fn parse_stock(args: &[String]) -> StockArgs {
-    let stock = args[0].clone();
-    let chart = args[1].clone();
-    let datalog = args[2].clone();
-    let currency = args[3].clone();
-
-    StockArgs {
-        stock,
-        chart,
-        datalog,
-        currency,
-    }
+    Ok(())
 }
